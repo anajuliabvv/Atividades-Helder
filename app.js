e7576f1f1ce331c1c75b02782ba2ed382d85ea6e
anajuliabvv <anajuliabvv@gmail.com>
2026-09-24 23:51:54 -0300

    core: criação de rota para listar tarefas

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ app.use(express.json());
 
 const PORT = 3000;
 
+const tarefas = [
+  { id: 1, titulo: "Estudar Express", concluida: false },
+  { id: 2, titulo: "Fazer atividade", concluida: true },
+  { id: 3, titulo: "Testar API", concluida: false },
+];
+
 app.get("/", (req, res) => {
   res.send("API de Tarefas no ar");
+});
+
+app.get("/tarefas", (req, res) => {
+  res.json(tarefas);
 });
 
 app.listen(PORT, () => {
